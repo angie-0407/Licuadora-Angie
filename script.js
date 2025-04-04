7f95d8edda62a3dc25298a31c2f87649fe1a3088
[angie-0407] <[angie.garcia@est.ulsa.edu.ni]>
2025-04-04 01:33:38 -0600

    Sonidos de licuadora implementados

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 console.log("Hola mundo desde JS");
 var estadoLicuadora = false;
 var licuadora = document.getElementById("blender");
+var sonidoLicuadora = document.getElementById("blender-sound");
+var botonLicuadora = document.getElementById("blender-button-sound");
 
 function controlarLicuadora() {
     if (estadoLicuadora == false) {
@@ -14,3 +16,12 @@ function controlarLicuadora() {
     }
   }
 
+function reproducirSonido() {
+    if (estadoLicuadora == true) {
+        sonidoLicuadora.play();
+        botonLicuadora.play();
+    }else {
+        sonidoLicuadora.pause();
+        botonLicuadora.pause();
+    }
+  }

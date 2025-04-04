5ced81294db132e1bd227c799d2af9b50b14fbf5
[angie-0407] <[angie.garcia@est.ulsa.edu.ni]>
2025-04-04 00:59:16 -0600

    Imagen de licuadora apagada

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log("Hola mundo desde JS");
 var estadoLicuadora = false;
+var licuadora = document.getElementById("blender");
 
 function controlarLicuadora() {
     if (estadoLicuadora == false) {
@@ -11,4 +12,3 @@ function controlarLicuadora() {
     }
   }
 
-  
